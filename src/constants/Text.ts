26e02aee70939d12494293d22ca663e09e5d4a47
Jaeyoung Kim <jaeyoungkim@Jaeyoungui-MacBookPro.local>
2025-11-14 19:24:01 +0900

    fix: Title 부분 반응형 관련  수정

--- src/constants/Text.ts
+++ src/constants/Text.ts
@@ -1,5 +1,6 @@
 export const MESSAGE = Object.freeze({
-    TITLE: "안녕하세요\n도전을 즐기며 성장하는\nFrontend Developer\n김재영 입니다.",
+    TITLE_DESKTOP: "안녕하세요\n도전을 즐기며 성장하는\nFrontend Developer\n김재영 입니다.",
+    TITLE_MOBILE: "안녕하세요\n도전을 즐기며\n성장하는\nFrontend Developer\n김재영 입니다.",
     MENUS: ['Profile', 'Skill', 'Project', 'Contact'],
     MY_PORTFOLIO: "My Portfolio",
     DFIZAE: "Dfizae",
--- src/pages/main/Title.tsx
+++ src/pages/main/Title.tsx
@@ -5,7 +5,12 @@ export default function Title(){
         <>
             <header className="flex w-full min-h-screen items-center justify-center bg-slate-200">
                 <h1 className="font-extrabold px-4 text-5xl md:text-7xl lg:text-8xl leading-[1.35] text-center whitespace-pre-line text-slate-900">
-                    {MESSAGE.TITLE}
+                    <span className="whitespace-pre-line hidden md:block">
+                        {MESSAGE.TITLE_DESKTOP}
+                    </span>
+                    <span className="whitespace-pre-line block md:hidden">
+                        {MESSAGE.TITLE_MOBILE}
+                    </span>
                 </h1>
             </header>
         </>
