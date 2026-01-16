d4e5a5cf2dee574676c4e8dcc65db4fca38f81ac
dfizae <gaza1268@naver.com>
2026-01-16 09:13:36 +0900

    프로필 이미지 수정

--- src/pages/main/Profile/ProfileImage.tsx
+++ src/pages/main/Profile/ProfileImage.tsx
@@ -4,7 +4,7 @@ export default function ProfileImage() {
     return(
         <>
             <div className="w-3/4 md:w-1/3 rounded-2xl overflow-hidden">
-                <img src="/images/pics/ProfilePic.png" alt= {IMAGE_INFO.PROFILE_ALT} className="w-full h-full object-cover" />
+                <img src="/images/pics/ProfilePic.jpg" alt= {IMAGE_INFO.PROFILE_ALT} className="w-full h-full object-cover" />
             </div>
         </>
     ); 
